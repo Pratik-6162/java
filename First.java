2c1baaf060d4972662194cd71019ba74138bf85d
Pratik-6162 <pratikvishwakarma748@gmail.com>
2024-12-27 03:22:16 +0530

    uploading First.java

---
+++ First.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class First{
+    public static void main(String[] args) {
+        //System.out.println("hello world");
+        String name = new Scanner(System.in).nextLine();
+        System.out.println(name);
+    }
+}
